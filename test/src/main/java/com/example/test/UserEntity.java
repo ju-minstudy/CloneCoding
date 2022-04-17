9050c0d2c5e4d88bb8b91ff0ec34b9badf837023
Jupiter-J <namju2912@gmail.com>
2022-04-17 19:34:07 +0900

    entity

---
+++ test/src/main/java/com/example/test/UserEntity.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "User_table")
+public class UserEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String gender;
+
+
+
+}
